1d717269d7e7de63cb9de87275cdf91f639297df
kamil <camgal04@gmail.com>
2025-10-03 00:49:44 +0300

    use jetty

--- src/main/java/ru/itmo/person_service/config/HttpsConfig.java
+++ src/main/java/ru/itmo/person_service/config/HttpsConfig.java
@@ -1,12 +1,9 @@
 package ru.itmo.person_service.config;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.eclipse.jetty.server.*;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,28 +17,19 @@ public class HttpsConfig {
     private int httpsPort;
 
     @Bean
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(redirectConnector());
-        return tomcat;
-    }
+    public WebServerFactoryCustomizer<JettyServletWebServerFactory> jettyCustomizer() {
+        return factory -> {
+            factory.addServerCustomizers(server -> {
+                HttpConfiguration httpConfig = new HttpConfiguration();
+                httpConfig.setSecureScheme("https");
+                httpConfig.setSecurePort(httpsPort);
+                httpConfig.addCustomizer(new SecureRequestCustomizer());
 
-    private Connector redirectConnector() {
-        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-        connector.setScheme("http");
-        connector.setPort(httpRedirectPort);
-        connector.setSecure(false);
-        connector.setRedirectPort(httpsPort);
-        return connector;
+                ServerConnector httpConnector = new ServerConnector(server, new HttpConnectionFactory(httpConfig));
+                httpConnector.setPort(httpRedirectPort);
+
+                server.addConnector(httpConnector);
+            });
+        };
     }
 }
